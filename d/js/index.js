394ce11549d46bddf756367215c3f00c947e2bda
张振衣 <413149044@qq.com>
2017-09-22 14:17:31 +0800

    add due, unit, background

--- d/js/index.js
+++ d/js/index.js
@@ -1,5 +1,11 @@
 if(!localStorage['Dancerphil-Dashboard']) {
-  localStorage['Dancerphil-Dashboard'] = `[{"id":1,"name":"俯卧撑","count":979,"history":[{"date":"2017-09-21","count":21}]},{"id":2,"name":"仰卧起坐","count":980,"history":[{"date":"2017-09-21","count":20}]},{"id":3,"name":"蹲起","count":969,"history":[{"date":"2017-09-21","count":31}]},{"id":4,"name":"挥剑","count":984,"history":[{"date":"2017-09-21","count":16}]}]`
+  const defaultValue = [
+    {"id":1,"name":"俯卧撑","count":979,"due":"2018-01-01","unit":"次","background":"pink","history":[{"date":"2017-09-21","count":21}]},
+    {"id":2,"name":"仰卧起坐","count":980,"due":"2018-01-01","unit":"次","background":"pink","history":[{"date":"2017-09-21","count":20}]},
+    {"id":3,"name":"蹲起","count":969,"due":"2018-01-01","unit":"次","background":"palegreen","history":[{"date":"2017-09-21","count":31}]},
+    {"id":4,"name":"挥剑","count":984,"due":"2018-01-01","unit":"次","background":"aliceblue","history":[{"date":"2017-09-21","count":16}]}
+  ]
+  localStorage['Dancerphil-Dashboard'] = JSON.stringify(defaultValue);
 }
 
 function getTodayHistory(item) {
@@ -22,23 +28,24 @@ function handleClick(id) {
 }
 
 const store = JSON.parse(localStorage['Dancerphil-Dashboard']);
-console.log(JSON.stringify(store));
 function render(store) {
   $('#playground').html('');
-  const rest = (new Date('2018-01-01').getTime() - new Date().getTime()) / 86400000;
-  $('#header').html(`今年还有 ${Math.floor(rest)} 天`);
   store.forEach(item => {
+    const rest = (new Date(item.due).getTime() - new Date().getTime()) / 86400000;
     const todayHistory = getTodayHistory(item) || {};
     const todayCount = todayHistory.count || 0;
-    let todayMission = item.count / rest - todayCount;
+    const mission = item.count / rest;
+    let todayMission = mission - todayCount;
     if (todayMission < 0) {
         todayMission = 0;
     }
     $('#playground').append(`
-      <div class="container" onclick="handleClick(${item.id})">
-        <div>任务名： ${item.name}</div>
-        <div>剩余： ${item.count}</div>
-        <div>今日任务： ${todayMission}</div>
+      <div class="container" style="background: ${item.background}" onclick="handleClick(${item.id})">
+        <div>${item.name}</div>
+        <div>剩余 ${item.count} ${item.unit}</div>
+        <div>今日任务 ${todayMission.toFixed(2)} ${item.unit}</div>
+        <div>明日任务 ${mission.toFixed(2)} ${item.unit}</div>
+        <div>还有 ${Math.floor(rest)} 天</div>
         <div>历史： ${JSON.stringify(item.history)}</div>
       </div>
     `);
