fe7ee553435e9a072c10c93c01ad38af9c5848d5
dancerphil <dancerphil1994@gmail.com>
2025-02-17 16:55:45 +0800

    revert wrong config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,10 @@
 import {defineConfig} from 'vite';
-import {resolve} from 'path';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
     return {
         build: {
             outDir: 'dist',
-            rollupOptions: {
-                input: {
-                    main: resolve(__dirname, 'index.html'),
-                    nested: resolve(__dirname, 'nested/index.html'),
-                },
-            },
         },
         optimizeDeps: {
             include: [
